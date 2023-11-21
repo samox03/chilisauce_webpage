49ad7e746d59b4d08d98d17cb01d8475a039725d
samox_ <samirasteinmayer@web.de>
2023-11-21 17:18:19 +0100

    edited navBar content distribution on wide screen

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -33,8 +33,8 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage}: Props) => {
                         {/* RIGHT SIDE */}
 
                         {isAboveMediumScreens ? (
-                        <div className={`${flexBetween} w-full`}>
-                            <div className={`${flexBetween} gap-8 text-sm`}>
+                        <div className={`${flexBetween}`}>
+                            <div className={`${flexBetween} gap-8 text-sm mr-8`}>
                                 <Link 
                                 page="Was bin ich"
                                 selectedPage={selectedPage}
