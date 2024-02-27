be85cf58b7883ce8cf010be1b4ea8a3679fbd4b6
samox_ <samirasteinmayer@web.de>
2024-02-27 22:50:32 +0100

    proceded footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,16 +1,49 @@
-import BG_half_redwavey from "@/assets//BG_half_redwavey.png";
+import ContactBtnLight from "@/shared/ContactBtnLight";
+import InstaBtnLight from "@/shared/InstaBtnLight";
+import Link from "../navbar/Link";
+import { SelectedPage } from "@/shared/types";
+
 
 
 type Props = {
-    setSelectedPage: (value: SelectedPage) => void;
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 }
 
-const Footer = ({ setSelectedPage }: Props) => {
-    const flexBetween = "flex items-center justify-between";
+const Footer = ({ selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
 
   return (
-    <div>
-        <img alt="red background wave" src={BG_half_redwavey} className="w-full"/>
+    <div className="footer-background">
+      {/* <img alt="red background wave" src={BG_half_redwavey} className="w-full" /> */}
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className={`${flexBetween} gap-8 flex-col`}>
+          <ContactBtnLight setSelectedPage={setSelectedPage}>Schreib mir</ContactBtnLight>
+          <InstaBtnLight setSelectedPage={setSelectedPage}>Folg mir auf Insta</InstaBtnLight>
+        </div>
+      </div>
+      <div className={`${flexBetween} gap-8 text-sm mr-8 text-white `}>
+        <Link
+          page="Was bin ich"
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+        <Link
+          page="Wer steckt dahinter"
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage} />
+        <Link
+          page="Wo gibts mich"
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+        />
+      </div>
+      <div>
+        <p><a>Impressum</a></p>
+        <p><a>Kontakt</a></p>
+      </div>
+      <div>
+      </div>
     </div>
   )
 }
---
+++ src/shared/InstaBtnLight.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { SelectedPage } from "./types";
+
+
+type Props = {
+    children: React.ReactNode;
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+const InstaBtnLight = ({children, setSelectedPage }: Props) => {
+  return (
+    <AnchorLink
+        className="rounded-lg border-solid border-2 bg-white border-gray-100 px-6 py-1 text-gray-500 shadow-3xl hover:text-gray-500 hover:font-semibold first-line:hover:border-solid hover:border-gray-500"
+        onClick={() => setSelectedPage(SelectedPage.Contact)}
+        href={`#${SelectedPage.Contact}`}
+    >
+        {children}
+    </AnchorLink>
+  )
+}
+
+export default InstaBtnLight
